1fca7df7fc70ba2172bcaddedf971ed8d5e9638b
samircd4 <samircd4@gmail.com>
2022-09-26 01:15:42 +0600

    Jazzmin Addmin customized

--- RealChat/settings.py
+++ RealChat/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Cutomize admin panel
+JAZZMIN_SETTINGS = {
+    'site_title': 'Real Time Chat App',
+    'site_header': 'Real Time Chat App',
+    'site_brand': 'Real Time Chat App',
+    # 'site_logo': 'static/image/logo.png',
+    'copyright': 'chatapp.com',
+}
